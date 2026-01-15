b8d46ec31c1776fec97b2a821fd5a2896a3a6d2c
alexis a secas <alexisreillo12@gmail.com>
2026-01-15 16:36:06 +0100

    Añadimos la variables de entorno

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,18 +2,24 @@ import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 import { PokemonModule } from './pokemon/pokemon.module';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
 
 @Module({
-  imports: [ServeStaticModule.forRoot({
-    rootPath: join(__dirname, '..', 'public')
-  }),
-  MongooseModule.forRoot("mongodb://localhost:27017/nest-pokemon"),
+  imports: [
+    ConfigModule.forRoot(),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public')
+    }),
+    MongooseModule.forRoot(process.env.MONGODB_URL!),
     PokemonModule,
     CommonModule,
     SeedModule]
 })
-export class AppModule { }
+export class AppModule {
+
+  constructor() { console.log(process.env) }
+}
